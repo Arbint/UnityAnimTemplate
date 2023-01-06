34f429fe093b10c5cd08652b9b5f823e5279341e
Arbint <cg.jingtianli@gmail.com>
2023-01-05 23:06:18 -0600

    added notes

--- Assets/Player/Player.cs
+++ Assets/Player/Player.cs
@@ -5,6 +5,17 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
+    [Header("How to use:" +
+        "\n1,Use the Movement Component to adjust the movement related values" +
+        "\n\n2,Use the Camera Controller to control camera location and trun speed" +
+        "\n\n3,Add the combo animations to the combos list in the Anim Event Component" +
+        "\nThe combo clear wait time for each combo is how log it waits after" +
+        "\na combo is fired before setting combo back to the first combo" + 
+        "\n\n4, Character Controller defines the actually collision of the player" +
+        "\n\n5, Anything you wish the player to walk on need to be in the ground layer" +
+        "\n")
+        ]
+    [SerializeField] [Multiline] string Note;
     PlayerInput playerInput;
 
     // Start is called before the first frame update
